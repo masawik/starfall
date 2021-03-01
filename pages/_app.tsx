182acc4117a5659df051f362d7e37369307828d5
Rochev Alexandr <velloso3@yandex.ru>
2021-03-01 11:40:34 +0300

    eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
-import {AppProps} from "next/app";
+import {AppProps} from 'next/app'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 
 export default function Home() {
   return (<h1>привет</h1>)
